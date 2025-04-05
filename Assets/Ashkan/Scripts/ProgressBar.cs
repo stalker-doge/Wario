c7ed296f513d51f743675e88f402c20e6da63717
Ashkan Soroor <100085031+AshkanSoro0r@users.noreply.github.com>
2025-04-05 20:18:20 +0100

    Next game title add to loading screen...

--- Assets/Ashkan/Scripts/ProgressBar.cs
+++ Assets/Ashkan/Scripts/ProgressBar.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +9,7 @@ public class ProgressBar : MonoBehaviour
 {
     public Slider progressBar;
     public float duration;
-    
+    public TMP_Text NextGameTitle;
     private bool isFinished;
     private float timer;
 
