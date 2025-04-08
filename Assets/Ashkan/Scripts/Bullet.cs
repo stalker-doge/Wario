10dcfd736c8dc130418701641a0cf69571a7caa1
mairajmbolton <mairajmuk@gmail.com>
2025-04-08 11:10:50 +0100

    Added Sounds to Aim & Shoot + SoundManager to Aim & Shoot Scene

--- Assets/Ashkan/Scripts/Bullet.cs
+++ Assets/Ashkan/Scripts/Bullet.cs
@@ -24,6 +24,7 @@ public class Bullet : MonoBehaviour
         //}
         if (Input.GetMouseButtonUp(0))
         {
+            SoundManager.Instance.ShootAudioClip();
             GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
             Vector2 dir = arrow.GetDirection();
 
@@ -33,6 +34,7 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        SoundManager.Instance.ProjectileBounceAudioClip();
         collisionCount++;
         if ( collisionCount == collisionCountMax)
         {
