446e163ba4d797fb7872ab8c11db3a4e46337ee7
stalker-doge <94372701+stalker-doge@users.noreply.github.com>
2025-04-28 14:07:05 +0100

    Golf Game !

---
+++ Assets/Lucian/Scripts/Golf/Golf Ball.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBall : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private GameObject golfBallPrefab;
+    [SerializeField]
+    private Transform shootPoint;
+
+    [SerializeField]
+    private ArrowController arrow;
+    public float bulletSpeed = 10f;
+
+    public int collisionCount = 0;
+    public int collisionCountMax = 2;
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            //SoundManager.Instance.ShootAudioClip();
+
+            GameObject bullet = Instantiate(golfBallPrefab, shootPoint.position, Quaternion.identity);
+            Vector2 dir = arrow.GetDirection();
+
+            bullet.GetComponent<Rigidbody2D>().velocity = dir * bulletSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //SoundManager.Instance.ProjectileBounceAudioClip();
+        collisionCount++;
+        if (collisionCount == collisionCountMax)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            //calls the game complete method from the score manager
+            ScoreManager scoreManager = FindObjectOfType<ScoreManager>();
+            if (scoreManager != null)
+            {
+                scoreManager.GameComplete();
+            }
+            else
+            {
+                Debug.LogError("ScoreManager not found in the scene.");
+            }
+        }
+    }
+}
---
+++ Assets/Lucian/Scripts/Golf/Golf.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golf : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private GameObject golfBall;
+
+    [SerializeField]
+    private Transform shootPoint;
+
+    [SerializeField]
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
