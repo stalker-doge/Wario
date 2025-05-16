e0bbe92f958176fa1abb74ec1be56d5e111c19bd
mairajmbolton <mairajmuk@gmail.com>
2025-05-16 17:11:36 +0100

    Drop Down Languages Added for Languages + LanguageManager Updated According + Scriptable Object Introduced to Handle Languages

---
+++ Assets/Mairaj/Scripts/LanguageData.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LanguageData", menuName = "ScriptableObjects/LanguageData")]
+public class LanguageData : ScriptableObject
+{
+    public Data [] data;
+
+    [Serializable]
+    public class Data
+    {
+        public string language;
+        public string languageCode;
+    }
+}
--- Assets/Mairaj/Scripts/LanguageManager.cs
+++ Assets/Mairaj/Scripts/LanguageManager.cs
@@ -3,12 +3,33 @@ using UnityEngine;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
 using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using System.Linq;
 
 public class LanguageManager : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_Dropdown myDropdown;
+
+    [SerializeField]
+    private LanguageData languageData;
+    private void Start()
+    {
+        myDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+    }
+
+    void OnDropdownValueChanged(int selectedIndex)
+    {
+        string selectedText = myDropdown.options[selectedIndex].text;
+
+        ChangeLanguage(languageData.data.Where(data => data.language == selectedText).FirstOrDefault().languageCode);
+    }
+
     // Call this to change language, e.g., "en", "es", "fr", etc.
     public void ChangeLanguage(string localeCode)
     {
+        Debug.Log("XYZ Code " + localeCode);
         StartCoroutine(SetLocale(localeCode));
     }
 
