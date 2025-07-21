78ec3c84bf501c848a67552e84e2f9840f8559ca
stalker-doge <94372701+stalker-doge@users.noreply.github.com>
2025-07-22 00:56:13 +0100

    Sort Size minigame

---
+++ Assets/Lucian/Scripts/Scripts/SortZone.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SortZone : MonoBehaviour, IDropHandler
+{
+    public float spacing = 120f; // Space between objects
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop called!");
+        var sortable = eventData.pointerDrag.GetComponent<SortableObject>();
+        if (sortable != null)
+        {
+            sortable.transform.SetParent(transform);
+            sortable.transform.SetAsLastSibling(); // Place at end of list
+            // Position will be set by ArrangeChildren
+            SortGameManager.Instance.CheckSortingAuto();
+            ArrangeChildren();
+        }
+    }
+
+    public void ArrangeChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i) as RectTransform;
+            if (child != null)
+            {
+                child.anchoredPosition = new Vector2(i * spacing, 0);
+            }
+        }
+    }
+} 
