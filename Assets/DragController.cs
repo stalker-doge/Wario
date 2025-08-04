6beead39d35031ed94eae859567bacfbacb94c11
Ashkan Soroor <100085031+AshkanSoro0r@users.noreply.github.com>
2025-08-04 21:03:52 +0100

    bug fix

--- Assets/DragController.cs
+++ Assets/DragController.cs
@@ -36,7 +36,7 @@ public class DragController : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
